f2492d1585fbf135a49b701d9469b318a2ad395b
shaycpm <shay@cherrypopmedia.com>
2020-10-27 15:11:51 +0200

    trying to fix unknown error

--- resources/js/src/layouts/components/navbar/components/ProfileDropDown.vue
+++ resources/js/src/layouts/components/navbar/components/ProfileDropDown.vue
@@ -16,41 +16,6 @@
             <vs-dropdown-menu class="vx-navbar-dropdown">
                 <ul style="min-width: 9rem">
 
-                    <!--                    <li-->
-                    <!--                            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"-->
-                    <!--                            @click="$router.push('/pages/profile').catch(() => {})">-->
-                    <!--                        <feather-icon icon="UserIcon" svgClasses="w-4 h-4"/>-->
-                    <!--                        <span class="ml-2">Profile</span>-->
-                    <!--                    </li>-->
-
-                    <!--                    <li-->
-                    <!--                            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"-->
-                    <!--                            @click="$router.push('/apps/email').catch(() => {})">-->
-                    <!--                        <feather-icon icon="MailIcon" svgClasses="w-4 h-4"/>-->
-                    <!--                        <span class="ml-2">Inbox</span>-->
-                    <!--                    </li>-->
-
-                    <!--                    <li-->
-                    <!--                            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"-->
-                    <!--                            @click="$router.push('/apps/todo').catch(() => {})">-->
-                    <!--                        <feather-icon icon="CheckSquareIcon" svgClasses="w-4 h-4"/>-->
-                    <!--                        <span class="ml-2">Tasks</span>-->
-                    <!--                    </li>-->
-
-                    <!--                    <li-->
-                    <!--                            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"-->
-                    <!--                            @click="$router.push('/apps/chat').catch(() => {})">-->
-                    <!--                        <feather-icon icon="MessageSquareIcon" svgClasses="w-4 h-4"/>-->
-                    <!--                        <span class="ml-2">Chat</span>-->
-                    <!--                    </li>-->
-
-                    <!--                    <li-->
-                    <!--                            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"-->
-                    <!--                            @click="$router.push('/apps/eCommerce/wish-list').catch(() => {})">-->
-                    <!--                        <feather-icon icon="HeartIcon" svgClasses="w-4 h-4"/>-->
-                    <!--                        <span class="ml-2">Wish List</span>-->
-                    <!--                    </li>-->
-
                     <vs-divider class="m-1"/>
 
                     <li
