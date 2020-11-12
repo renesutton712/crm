57dce2194f978169032a3b2068e60be18e1a4c1a
shaycpm <shay@cherrypopmedia.com>
2020-11-12 18:55:43 +0200

    Added dropdown table leads

--- resources/js/src/views/pages/leads/IndexComponent.vue
+++ resources/js/src/views/pages/leads/IndexComponent.vue
@@ -11,7 +11,7 @@
         </vs-row>
         <vs-row>
             <vs-col>
-                <vs-table v-model="selected_leads" search :data="leads_list" multiple>
+                <vs-table v-model="selected_leads" search :data="leads_list">
                     <template slot="header"></template>
                     <template slot="thead">
                         <vs-th sort-key="unique_id">ID</vs-th>
@@ -22,12 +22,15 @@
                         <vs-th sort-key="first_name">First Name</vs-th>
                         <vs-th sort-key="last_name">Last Name</vs-th>
                         <vs-th sort-key="email">Email</vs-th>
+                        <vs-th sort-key="prefix">Prefix</vs-th>
                         <vs-th sort-key="phone">Phone</vs-th>
-                        <vs-th sort-key="ip">IP</vs-th>
-                        <vs-th sort-key="ua">UA</vs-th>
+                        <!--                        <vs-th sort-key="ip">IP</vs-th>-->
+                        <!--                        <vs-th sort-key="ua">UA</vs-th>-->
                         <vs-th sort-key="status">Type</vs-th>
-                        <vs-th sort-key="url_params">URL Params</vs-th>
-                        <vs-th sort-key="url_params">Network Response</vs-th>
+                        <!--                        <vs-th sort-key="url_params">URL Params</vs-th>-->
+                        <vs-th sort-key="network_response">Network Error</vs-th>
+                        <!--                        <vs-th sort-key="created_at">Created</vs-th>-->
+                        <!--                        <vs-th sort-key="updated_at">Updated</vs-th>-->
                     </template>
                     <template slot-scope="{data}">
                         <vs-tr :data="tr" :key="indextr" v-for="(tr,indextr) in data">
@@ -48,17 +51,30 @@
                             <vs-td :data="data[indextr].first_name">{{data[indextr].first_name}}</vs-td>
                             <vs-td :data="data[indextr].last_name">{{data[indextr].last_name}}</vs-td>
                             <vs-td :data="data[indextr].email">{{data[indextr].email}}</vs-td>
+                            <vs-td :data="data[indextr].prefix">{{data[indextr].prefix}}</vs-td>
                             <vs-td :data="data[indextr].phone">{{data[indextr].phone}}</vs-td>
-                            <vs-td :data="data[indextr].ip">{{data[indextr].ip}}</vs-td>
-                            <vs-td :data="data[indextr].ua">{{data[indextr].ua}}</vs-td>
-                            <vs-td v-if="data[indextr].status === 3" class="font-bold text-primary" :data="data[indextr].status">
+                            <!--                            <vs-td :data="data[indextr].ip">{{data[indextr].ip}}</vs-td>-->
+                            <!--                            <vs-td :data="data[indextr].ua">{{data[indextr].ua}}</vs-td>-->
+                            <vs-td v-if="data[indextr].status === 3" class="font-bold text-success" :data="data[indextr].status">
                                 FTD
                             </vs-td>
-                            <vs-td v-else :class="[data[indextr].status === 1 ? 'text-dark font-bold' : 'text-success font-bold']"
+                            <vs-td v-else :class="[data[indextr].status === 1 ? 'text-dark' : 'text-primary font-bold']"
                                    :data="data[indextr].status">{{data[indextr].status === 1 ? 'Click' : 'Lead'}}
                             </vs-td>
-                            <vs-td :data="data[indextr].url_params">{{data[indextr].url_params}}</vs-td>
+                            <!--                            <vs-td :data="data[indextr].url_params">{{data[indextr].url_params}}</vs-td>-->
                             <vs-td :datafld="data[indextr].network_response">{{data[indextr].network_response}}</vs-td>
+                            <!--                            <vs-td :datafld="data[indextr].created_at">{{data[indextr].created_at}}</vs-td>-->
+                            <!--                            <vs-td :datafld="data[indextr].updated_at">{{data[indextr].updated_at}}</vs-td>-->
+                            <template class="expand-user" slot="expand">
+                                <vs-list>
+                                    <vs-list-item title="URL params"
+                                                  :subtitle="JSON.parse(data[indextr].url_params)"></vs-list-item>
+                                    <vs-list-item title="UA" :subtitle="data[indextr].ua"></vs-list-item>
+                                    <vs-list-item title="IP" :subtitle="data[indextr].ip"></vs-list-item>
+                                    <vs-list-item title="Created" :subtitle="data[indextr].created_at"></vs-list-item>
+                                    <vs-list-item title="Updated" :subtitle="data[indextr].updated_at"></vs-list-item>
+                                </vs-list>
+                            </template>
                         </vs-tr>
                     </template>
                 </vs-table>
@@ -108,5 +124,4 @@
 </script>
 
 <style scoped>
-
 </style>
