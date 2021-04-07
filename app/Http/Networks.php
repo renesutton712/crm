ada279b1e600e6903b1ed0de498fd1bbebf80405
shaycpm <shay@cherrypopmedia.com>
2021-04-07 11:37:16 +0300

    Hot Networks.php fix

--- app/Http/Networks.php
+++ app/Http/Networks.php
@@ -17,8 +17,8 @@ use GuzzleHttp\Exception\GuzzleException;
 class Networks {
 
     public function networksMap($network, $params) {
-        $network_id = $network instanceof \stdClass ? (int)$network->network_id : (int)$network;
-        switch ($network_id) {
+//        $network_id = $network instanceof \stdClass ? (int)$network->network_id : (int)$network;
+        switch ($network->network_id) {
             case 7:
                 $cod = new COD();
                 return $cod->registerLeadGet($params);
