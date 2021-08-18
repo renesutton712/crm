a984c10c919c1d10705f22f5a695504833f21316
shaycpm <shay@cherrypopmedia.com>
2021-08-18 10:15:28 +0300

    Fixing on convertick

--- app/Http/Networks/Convertick.php
+++ app/Http/Networks/Convertick.php
@@ -63,8 +63,10 @@ class Convertick extends NetworkFactory {
             }
             $this->storeNetworkResponse($unique_id, $data['data']['signupRequestID']);
             $broker_res = $this->brokerAutoLoginUrl($data['data']['signupRequestID']);
-            if (!$broker_res['status']) {
-                throw new \Exception($broker_res['msg']);
+            throw new \Exception(json_encode($broker_res));
+            if (!isset($broker_res['status'])) {
+                throw new \Exception(json_encode($broker_res));
+//                throw new \Exception($broker_res['msg']);
             }
             $response = ['status' => true, 'msg' => $broker_res['msg']];
             $iframe = $this->getIframePixel($camp_id);
