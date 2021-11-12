1fd98eb82af25efe8da418ce988788720d917b86
Stas <stas4000@gmail.com>
2021-11-12 09:17:14 +0200

    Fixing memory limit bug

--- public/index.php
+++ public/index.php
@@ -49,6 +49,11 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+ini_set('memory_limit', '1024M');
+
+
+
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
