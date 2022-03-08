a1696b4487df5be34c6c8ad3e70c80a360a681d7
Stas Sorokin <stas4000@gmail.com>
2022-03-08 11:31:21 +0200

    Getting eletrahub token

--- app/Http/Networks/electrohub-login.php
+++ app/Http/Networks/electrohub-login.php
@@ -1,8 +1,8 @@
 <?php
 
+require __DIR__ . '../../../vendor/autoload.php';
 use GuzzleHttp\Client;
 
-require __DIR__ . '../../../vendor/autoload.php';
 
 function login() {
     $client = new Client();
